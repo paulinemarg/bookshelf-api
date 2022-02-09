6ea999ab41bef8496b7694fdce1be3445264a84b
paulinemarg <paulinemargossian@gmail.com>
2022-02-09 22:56:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser'),
+ express = require('express'),
+ morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('My app is listening on port 8080.');
+});
