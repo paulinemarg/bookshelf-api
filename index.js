816b6b05b92d4610bdcb94c338256c3ab1651519
paulinemarg <paulinemargossian@gmail.com>
2022-06-21 21:23:11 +0200

    added user requests

--- index.js
+++ index.js
@@ -144,6 +144,101 @@ app.get('/genres/:Name', (req, res) => {
     });
 });
 
+//------------------User Requests---------------//
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    }).catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({
+    Username: req.params.Username
+  })
+    .then((user) => {
+      res.json(user);
+    }).catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.post('/users', (req, res) => {
+  Users.findOne({
+    Username: req.body.Username 
+  })
+  .then((user) => {
+    if (user) { 
+      return res.status(400).send(req.body.Username + ' already exists!');
+    } else {
+      Users
+        .create({
+          Username: req.body.Username,
+          Password: hashedPasswod,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }).then((user) => {
+          res.status(201).json(user)
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+  }).catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
+});
+
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({
+    Username: req.params.Username
+  }, {
+    $set: {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  }, {
+    new: true
+  }, //this line makes sure that updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndDelete({
+    Username: req.params.Username
+  }).then((user) => {
+    if (!user) {
+      res.status(400).send(req.params.Username + ' was not found!');
+    } else {
+      res.status(200).send(req.params.Username + ' was removed!');
+    }
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 app.listen(8080, () => {
   console.log('My app is listening on port 8080.');
 });
