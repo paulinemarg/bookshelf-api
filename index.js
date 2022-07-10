6ff84cc77945be3bbca12754e8072de3e4ffc4b1
paulinemarg <paulinemargossian@gmail.com>
2022-07-10 12:35:03 +0200

    required cors

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const Models = require('./models.js');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
 
 const app = express();
 const { check, validationResult } = require('express-validator');
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ 
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const Books = Models.Book;
 const Users = Models.User;
